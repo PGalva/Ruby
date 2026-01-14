f7f7c6f8d132d943b81dff31a0dffb370b1e7da9
Pedro Galvao Barbosa <pedrogalvaobarbosa@gmail.com>
2026-01-13 22:43:16 -0300

    condicao de ifs

--- translator.rb
+++ translator.rb
@@ -11,22 +11,35 @@ Translations = {
 };
 
 def translate(word) #word é o parâmetro
-    if Translations.key?(word) #key? verifica se a chave existe no dicionário
-        return Translations[word]
-    else
-        return "word not found"
-    end
+    #Translations.key?(word) #key? verifica se a chave existe no dicionário
+    return Translations[word]
 end
 
 def get_the_word 
     puts "Enter a word to translate:"
     word = gets.chomp()
+end
+
+def validate_words(word)
+  if Translations.key?(word)
     puts translate(word)
+    else
+  puts "Sorry, the word '#{word}' is not in the dictionary."
+  exit
+end
+end
+
+
+def show_translation(word) #Motivo da função é separar a lógica de obtenção da palavra e exibição da tradução
+    translation = translate(word)
+    validate_words(word) #chamando a função validate_words
+    puts "The translation is: #{translation}" #interpolação de strings
 end
 
 while true
-  get_the_word
-  puts "Do you want to translate another word? (yes/no)"
+  word = get_the_word #obtendo a palavra do usuário
+  show_translation(word) #mostrando a tradução
+  puts "Do you want to translate another word? (yes/no)" #perguntando ao usuário se deseja continuar
   answer = gets.chomp().downcase #downcase para normalizar a resposta
   break if answer != "yes"
 end
