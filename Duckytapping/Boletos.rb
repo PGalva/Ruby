8fe56ac800ce5948f1205efc0a5c766e484d8d00
Pedro Galvao Barbosa <pedrogalvaobarbosa@gmail.com>
2026-08-26 23:31:52 -0300

    Add the method in Boletos class

--- Duckytapping/Boletos.rb
+++ Duckytapping/Boletos.rb
@@ -1,11 +1,13 @@
+require_relative 'Documentos'
 class Boletos 
 
   def pagar(valor)
     puts "Pagamento realizado com sucesso"
   end
+
+  def gerar_impressao(papel)
+    papel.exportar
+  end
 end
 
 
-def processar(pagamento, valor)
-  pagamento.pagar
-end
