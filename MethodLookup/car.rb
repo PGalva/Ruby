4617b1f6377d9e0c7ceede0ad3e631356978ca4e
Pedro Galvao Barbosa <pedrogalvaobarbosa@gmail.com>
2026-09-22 22:03:45 -0300

    changing the hierarchy between Transport and car

--- MethodLookup/car.rb
+++ MethodLookup/car.rb
@@ -1,13 +1,11 @@
-class Car
-  attr_accessor :make, :model, :year
+require_relative 'Transport'
 
-  def initialize(make, model, year)
-    @make = make
-    @model = model
-    @year = year
-  end
-
+class Car < Transport
+  
   def start_engine
     puts "Starting the engine of the #{@year} #{@make} #{@model}."
   end
 end
+
+fusca = Transport.new("Volkswagen", "Fusca", 1970)
+fusca.start_engine
